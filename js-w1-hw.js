458b1186b196844d96de9f9baed5ab37b5a88cc8
Aleksandr Buniatov <abuniatov@gmail.com>
2024-06-05 12:56:39 +0200

    Created two variables called yearOfBirth and yearFuture. Assigned those to my own age and a random year in the future. Using yearOfBirth and yearFuture calculated the age and stored that in a variable called age. Loged out the string.

---
+++ js-w1-hw.js
@@ -0,0 +1,6 @@
+// Age-ify (A future age calculator)
+
+const yearOfBirth = 1986;
+const yearFuture = 2069;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
