c7ecc519d8b385bbf91ab237c5ed93bd243e3251
abuniatov <abuniatov@gmail.com>
2024-06-30 09:49:05 +0200

    added the Greeting part

---
+++ js-w4-hw.js
@@ -0,0 +1,19 @@
+const voiceAssistant = (function () {
+  let userName = "";
+  let todos = [];
+  let timers = {};
+
+  function getReply(command) {
+    command = command.toLowerCase().trim();
+    // Greeting
+    if (command.startsWith("hello my name is ")) {
+      const name = command.replace("hello my name is ", "").trim();
+      if (userName === name) {
+        return `You have already introduced yourself as ${userName}.`;
+      } else {
+        userName = name;
+        return `Nice to meet you, ${userName.charAt(0).toUpperCase() + userName.slice(1)}.`;
+      }
+    }
+
+    
