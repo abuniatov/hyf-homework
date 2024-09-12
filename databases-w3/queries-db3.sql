cd4e475c676fd8f7edaa900eef54d212038d9e2f
abuniatov <abuniatov@gmail.com>
2024-09-12 22:44:28 +0200

    added Review queries

--- databases-w3/queries-db3.sql
+++ databases-w3/queries-db3.sql
@@ -29,3 +29,19 @@ UPDATE reservation
 SET number_of_guests = 3, meal_id = 1, created_date = '2024-9-15', contact_phonenumber = '50123457', contact_name = 'John Malkovich', contact_email = 'jmalkovich@gmail.com'
 WHERE id = 1;
 DELETE FROM reservation WHERE id = 1;
+
+-- 3. Review queries:
+-- Get all reviews
+-- Add a new review
+-- Get a review with any id, fx 1
+-- Update a review with any id, fx 1. Update any attribute fx the title or multiple attributes
+-- Delete a review with any id, fx 1
+
+SELECT * FROM review;
+INSERT INTO review (title, description, meal_id, stars, created_date)
+VALUES ('Tuna Sandwich review', 'The sandwich was so so...', 1, 3, '2024-9-12');
+SELECT * FROM review WHERE id = 1;
+UPDATE review
+SET title = 'Tuna Sandwich updated review', description = 'I actually reconsidered and decided that the sandwich was really good', meal_id = 1, stars = 5, created_date = '2024-9-13'
+WHERE id = 1;
+DELETE FROM review WHERE id = 1
